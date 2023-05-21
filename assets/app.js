e316b3b665e0b48ca7a19f6b073eee5f38f78d41
LordNinth <rr621801@gmail.com>
2023-05-22 00:12:37 +0100

    basic function

--- assets/app.js
+++ assets/app.js
@@ -3,20 +3,65 @@ const exerciseInput = document.querySelector(".exerciseInput");
 const recipeInput = document.querySelector(".recipeInput")
 const exBtn = document.querySelector(".exSearch")
 const recipeBtn = document.querySelector(".recipeSearch")
-const apiUrl = 'https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=${params}'
+// const apiUrl = 'https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=${params}'
+
+//exercise api
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f949d10bd8mshf91fea6233b3069p123fb3jsn202554e50322',
+		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+	}
+};
 
 //recipe api
-const url1 = 'https://edamam-recipe-search.p.rapidapi.com/search?q=${params}'
-const options = {
+const options1 = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'f949d10bd8mshf91fea6233b3069p123fb3jsn202554e50322',
 		'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
 	}
+	
+
 };
+// //call exercise
+async function checkEx(body) {
+	const response = await fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${body}`, options);
+	const data = await response.json();
+	console.log(data);
+
+	//ex1
+	document.querySelector(".exName").innerHTML = data[0].name;
+	// document.querySelector(".exTarget").innerHTML = data[0].target
+	// document.querySelector(".exEquipment").innerHTML = data[0].equipment;
+	// document.querySelector(".exUrl").innerHTML = data[0].gifUrl;
+
+	//ex2
+	document.querySelector(".exName1").innerHTML = data[1].name;
+	// document.querySelector(".exTarget1").innerHTML = data[1].target
+	// document.querySelector(".exEquipment1").innerHTML = data[1].equipment;
+	// document.querySelector(".exUrl1").innerHTML = data[1].gifUrl;
+
+	//ex3
+	document.querySelector(".exName2").innerHTML = data[2].name;
+	// document.querySelector(".exTarget2").innerHTML = data[2].target
+	// document.querySelector(".exEquipment2").innerHTML = data[2].equipment;
+	// document.querySelector(".exUrl2").innerHTML = data[2].gifUrl;
+	
+	//NOTE: Dont uncomment these, free subcription only lets you call so many times before hitting daily limit!!!!!!!!!!!!!!!!!
+	
+}
+
+//call function
+exBtn.addEventListener('click', ()=> {
+	checkEx(exerciseInput.value);
+})
+
+
+
 //call recipe
 async function checkRecipe(params) {
-	const response = await fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${params}`, options); 
+	const response = await fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${params}`, options1); 
 	const data1 = await response.json();
 	console.log(data1);
 	// recipe1
@@ -61,59 +106,6 @@ recipeBtn.addEventListener('click', ()=> {
 // 	}
 // };
 
-//exercise api
-const options1 = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'f949d10bd8mshf91fea6233b3069p123fb3jsn202554e50322',
-		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-	}
-};
-
-
-// //call exercise
-async function checkEx(body) {
-	const response = await fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${body}`, options1);
-	const data = await response.json();
-	console.log(data);
-
-	//ex1
-	document.querySelector(".exName").innerHTML = data[0].name;
-	// document.querySelector(".exTarget").innerHTML = data[0].target
-	// document.querySelector(".exEquipment").innerHTML = data[0].equipment;
-	// document.querySelector(".exUrl").innerHTML = data[0].gifUrl;
-
-	//ex2
-	document.querySelector(".exName1").innerHTML = data[1].name;
-	// document.querySelector(".exTarget1").innerHTML = data[1].target
-	// document.querySelector(".exEquipment1").innerHTML = data[1].equipment;
-	// document.querySelector(".exUrl1").innerHTML = data[1].gifUrl;
-
-	//ex3
-	document.querySelector(".exName2").innerHTML = data[2].name;
-	// document.querySelector(".exTarget2").innerHTML = data[2].target
-	// document.querySelector(".exEquipment2").innerHTML = data[2].equipment;
-	// document.querySelector(".exUrl2").innerHTML = data[2].gifUrl;
-	
-	
-	
-}
-
-//call function
-exBtn.addEventListener('click', ()=> {
-	checkEx(exerciseInput.value);
-})
-
-// fetch(`https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=${params}`, options)
-// 		.then(response => response.json())
-// 		.then(response =>  console.log(response))
-// 		.then(response =>  {
-// 			console.log(response[0].Force)
-// 		})
-
-
-
-// })
 
 
 
@@ -157,48 +149,43 @@ exBtn.addEventListener('click', ()=> {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //ROSH NOTE: DONT TOUCH THIS, THIS IS ANOTHER EXERCISE API BUT DAILY LIMIT EXPIRED.
 // const url = 'https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=pectoralis%20major';
 // const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'f949d10bd8mshf91fea6233b3069p123fb3jsn202554e50322',
-// 		'X-RapidAPI-Host': 'exerciseapi3.p.rapidapi.com'
-// 	}
-// };
-
-// let params = ''
-// const callFunc = () => {
-// 	params = exerciseInput.value
-// 	fetch(`https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=${params}`, options)
-// 		.then(response => response.json())
-// 		.then(response =>  console.log(response))
-// 		.then(response =>  {
-// 			console.log(response[0].Force)
-// 		})
-			
-			
-			
-
-
-
-
-
-		// .then(data => {
-		// 	let output ='';
-		// 	data.name.map(item =>{
-		// 		output += `
-		// 		<h1>${data.name}</h1>
-		// 		`;
-
-		// 	})
-		// 	document.querySelector('.exercises1').innerHTML = output;
-		 
-	// 	.catch(err => console.error(err));
-	// exerciseInput.value = '';
-	// }
-
-
-
-
-
-// btn.addEventListener("click", callFunc )
+	// 	method: 'GET',
+	// 	headers: {
+		// 		'X-RapidAPI-Key': 'f949d10bd8mshf91fea6233b3069p123fb3jsn202554e50322',
+		// 		'X-RapidAPI-Host': 'exerciseapi3.p.rapidapi.com'
+		// 	}
+		// };
+		
+		
+		
+		// fetch(`https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=${params}`, options)
+		// 		.then(response => response.json())
+		// 		.then(response =>  console.log(response))
+		// 		.then(response =>  {
+		// 			console.log(response[0].Force)
+		// 		})
+		
+		
+		
+		// })
+		
