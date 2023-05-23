80a60484bacec25b29405d557a7b533d9e916a94
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-05-23 18:47:18 +0100

    Added Javascript Code for the calculator button

--- assets/app.js
+++ assets/app.js
@@ -95,17 +95,68 @@ recipeBtn.addEventListener('click', ()=> {
 
 
 
+//---------------------------------- Code from Francis Starts Here--------------------------------------------------
+//Get userinputs
+const ageSpinner = document.querySelector('#ageSpinner')
+const genderVal = document.querySelector('#gender')
+const weightSpinner = document.querySelector('#weightSpinner')
+const heightSpinner = document.querySelector('#heightSpinner')
+const goBtn = document.querySelector('.goBtn')
 
-//exercise api
-// const url = 'https://exerciseapi3.p.rapidapi.com/search/?primaryMuscle=pectoralis%20major';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'f949d10bd8mshf91fea6233b3069p123fb3jsn202554e50322',
-// 		'X-RapidAPI-Host': 'exerciseapi3.p.rapidapi.com'
-// 	}
-// };
+//get display labels
+const bmiContainer = document.querySelector('.bmiData')
+const statusContainer = document.querySelector('.statusData')
+//fetch data
+async function fetchHealthData(age,gender,height,weight){
+  // const gender ="male"
+	// const age =47; //replace with input field from the web app
+	// const height=174; //replace with input field from the web app
+  // const weight =81;
+  // Get user inputs
+	const apiKey ='45a321c547mshbf41430848e0da1p17077ejsn7669b6d7e3c4'
+	const url = `https://health-calculator-api.p.rapidapi.com/bodyfat?gender=${gender}&age=${age}&height=${height}&weight=${weight}&unit=metric`;
+  
 
+	const options = {
+                    method: 'GET',
+                    headers: {
+                      'X-RapidAPI-Key': apiKey,
+                      'X-RapidAPI-Host': 'health-calculator-api.p.rapidapi.com'
+		                        }
+	                };
+	  try {
+          const response = await fetch(url, options);
+          const result = await response.json();
+
+          
+        
+          // Display results
+          displayHealthData(result)
+	     } 
+    catch (error) {
+		            console.error(error);
+	     }
+	}
+
+  
+  function displayHealthData(result) {
+    //extract data
+    console.log(result)
+    const bmi = result.bmi
+    const status  = result.bodyfat_status
+      // Update the display labels with the extracted values
+      bmiContainer.textContent = `${bmi}`;
+      statusContainer.textContent = `${status}`;
+  }
+  // Code for the GO button
+  goBtn.addEventListener('click',() => {
+    const age = ageSpinner.value;
+    const gender = genderVal.value;
+    const height = heightSpinner.value;
+    const weight = weightSpinner.value;
+    console.log(`age:${ageSpinner.value}|gender:${genderVal.value} |height:${heightSpinner.value}|weight:${weightSpinner.value}`)//testing getting user input
+    fetchHealthData(age,gender,height,weight);
+  })
 
 
 
